bb8ab07b477b8de7175db24a04e1a3569028da98
Michael Stapelberg <michael@stapelberg.de>
2020-08-09 10:14:42 +0200

    MK66F18: use bkpt instruction to jump to bootloader
    The MK66F18 is part of the kinetis family of devices,
    so with this change we will use bkpt like explained in
    https://forum.pjrc.com/threads/30521-Rebooting-Teensy-(LC-3-0)-to-bootloader-without-Teensyduino
    and done in
    https://github.com/PaulStoffregen/cores/blob/784bf775c8bcd80fe2433332d5b706bf1be3c107/teensy3/usb_dev.c#L894
    
    related to https://github.com/kinx-project/kint/issues/7

--- tmk_core/common/chibios/bootloader.c
+++ tmk_core/common/chibios/bootloader.c
@@ -41,7 +41,7 @@ void enter_bootloader_mode_if_requested(void) {
     }
 }
 
-#elif defined(KL2x) || defined(K20x) /* STM32_BOOTLOADER_ADDRESS */
+#elif defined(KL2x) || defined(K20x) || defined(MK66F18) /* STM32_BOOTLOADER_ADDRESS */
 /* Kinetis */
 
 #    if defined(KIIBOHD_BOOTLOADER)
